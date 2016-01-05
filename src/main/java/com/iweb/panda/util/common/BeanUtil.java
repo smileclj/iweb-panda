3d013cd589b7b795b466308c910fa8d209ef86fd
smileclj <842705148@qq.com>
2016-01-05 13:05:13 +0800

    xx

--- src/main/java/com/iweb/panda/util/common/BeanUtil.java
+++ src/main/java/com/iweb/panda/util/common/BeanUtil.java
@@ -1,5 +1,22 @@
 package com.iweb.panda.util.common;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BeanUtil {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(BeanUtil.class);
+
+    public static void copyProperties(Object dest, Object orig) throws IllegalAccessException,
+                                                               InvocationTargetException {
+        try {
+            BeanUtils.copyProperties(dest, orig);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            logger.error("对象拷贝失败", e);
+            throw e;
+        }
+    }
 }
