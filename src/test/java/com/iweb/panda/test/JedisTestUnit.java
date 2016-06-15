fd47e63db303e881d908b77b47f7a9238eb2737b
smileclj <842705148@qq.com>
2016-06-15 20:09:25 +0800

    xx

--- src/test/java/com/iweb/panda/test/JedisTestUnit.java
+++ src/test/java/com/iweb/panda/test/JedisTestUnit.java
@@ -1,23 +1,37 @@
 package com.iweb.panda.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class JedisTestUnit {
+	private JedisPool pool;
+	private Jedis jedis;
 
-    @Test
-    public void test1() {
-//        Jedis jedis = RedisUtil.getJedis();
-//        if (jedis != null) {
-//            jedis.set("name", "小明");
-//            System.out.println(jedis.get("name"));
-//        }
-    }
+	@Before
+	public void init() {
+		// 默认是0号库
+		JedisPoolConfig config = new JedisPoolConfig();
+		pool = new JedisPool(config, "192.168.1.9", 6379, 2000, "123456");
+		jedis = pool.getResource();
+	}
 
-    @Test
-    public void info() {
-        // Jedis jedis = RedisUtil.getJedis();
-        // if (jedis != null) {
-        // System.out.println(jedis.info());
-        // }
-    }
+	@Test
+	public void set_get() {
+		jedis.set("name", "小明");
+		System.out.println(jedis.get("name"));
+	}
+	
+	@Test
+	public void get() {
+		System.out.println(jedis.get("name"));
+	}
+
+	@Test
+	public void info() {
+		System.out.println(jedis.info());
+	}
 }
