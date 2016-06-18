5ecac173555996ac95e67efa63c64e756afa99ae
smileclj <842705148@qq.com>
2016-06-18 10:27:28 +0800

    x

--- src/test/java/com/iweb/panda/test/JedisTestUnit.java
+++ src/test/java/com/iweb/panda/test/JedisTestUnit.java
@@ -15,16 +15,16 @@ public class JedisTestUnit {
 	public void init() {
 		// 默认是0号库
 		JedisPoolConfig config = new JedisPoolConfig();
-		pool = new JedisPool(config, "192.168.1.9", 6379, 2000, "123456");
+		pool = new JedisPool(config, "192.168.1.32", 6379, 2000, "123456");
 		jedis = pool.getResource();
 	}
 
 	@Test
 	public void set_get() {
-		jedis.set("name", "小明");
-		System.out.println(jedis.get("name"));
+		jedis.set("count", "0");
+		System.out.println(jedis.get("count"));
 	}
-	
+
 	@Test
 	public void get() {
 		System.out.println(jedis.get("name"));
