36ddb97410ef0bb410f3dec1d6a695887224bef6
smileclj <842705148@qq.com>
2016-07-12 10:17:20 +0800

    x

---
+++ src/test/java/com/panda/iweb/test/suggest/Test_9.java
@@ -0,0 +1,25 @@
+package com.panda.iweb.test.suggest;
+
+import java.util.List;
+
+public class Test_9 {
+	public void c(Integer a) {
+
+	}
+
+	public void c(String a) {
+
+	}
+
+	// public String c(String c) {
+	// return "";
+	// }
+
+	public static String a(List<Integer> list) {
+		return "";
+	}
+
+	// public static Integer a(List<String> list) {
+	// return 1;
+	// }
+}
