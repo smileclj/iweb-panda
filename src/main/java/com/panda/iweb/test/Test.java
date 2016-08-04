ad1512578ffb526d8c5df3c2275947b6e2b0e2f4
huixiangdou <huixiangdou@2dfire.com>
2016-08-04 18:51:28 +0800

    add test

---
+++ src/main/java/com/panda/iweb/test/Test.java
@@ -0,0 +1,25 @@
+package com.panda.iweb.test;
+
+/**
+ * Created by Administrator on 2016/8/4.
+ */
+public class Test {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
