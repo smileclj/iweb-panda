acce7e9195aaa4c6584234570d257a71f5b743a3
huixiangdou <huixiangdou@2dfire.com>
2016-08-17 14:08:38 +0800

    daily commit

--- src/main/java/com/panda/iweb/test/AddTestForMerge.java
+++ src/main/java/com/panda/iweb/test/AddTestForMerge.java
@@ -4,4 +4,22 @@ package com.panda.iweb.test;
  * Created by huixiangdou on 2016/8/17.
  */
 public class AddTestForMerge {
+    private Integer subId;
+    private String subName;
+
+    public Integer getSubId() {
+        return subId;
+    }
+
+    public void setSubId(Integer subId) {
+        this.subId = subId;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
 }
