36d195cc065d9199c9b6f9873446a7a98e2c59a6
huixiangdou <huixiangdou@2dfire.com>
2016-08-17 14:07:47 +0800

    daily commit

--- src/main/java/com/panda/iweb/test/AddTestForMerge.java
+++ src/main/java/com/panda/iweb/test/AddTestForMerge.java
@@ -4,4 +4,22 @@ package com.panda.iweb.test;
  * Created by huixiangdou on 2016/8/17.
  */
 public class AddTestForMerge {
+    private Integer mainId;
+    private String mainName;
+
+    public Integer getMainId() {
+        return mainId;
+    }
+
+    public void setMainId(Integer mainId) {
+        this.mainId = mainId;
+    }
+
+    public String getMainName() {
+        return mainName;
+    }
+
+    public void setMainName(String mainName) {
+        this.mainName = mainName;
+    }
 }
