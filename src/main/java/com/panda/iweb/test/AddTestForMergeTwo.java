0d2949b2a19288b0658e74f6dbe630c80bbc0633
huixiangdou <huixiangdou@2dfire.com>
2016-08-19 15:56:33 +0800

    AddTestForMergeTwo modify

--- src/main/java/com/panda/iweb/test/AddTestForMergeTwo.java
+++ src/main/java/com/panda/iweb/test/AddTestForMergeTwo.java
@@ -10,5 +10,7 @@ public class AddTestForMergeTwo {
     public void method(){
         System.out.println(111);
         System.out.println(222);
+        System.out.println(333);
+        System.out.println(444);
     }
 }
