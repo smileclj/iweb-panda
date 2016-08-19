48b8c5f0776a0c0a487faa23e8ceb2cbe55efabd
huixiangdou <huixiangdou@2dfire.com>
2016-08-19 14:29:38 +0800

    AddTestForMegreTwo modify

--- src/main/java/com/panda/iweb/test/AddTestForMergeTwo.java
+++ src/main/java/com/panda/iweb/test/AddTestForMergeTwo.java
@@ -4,4 +4,8 @@ package com.panda.iweb.test;
  * Created by huixiangdou on 2016/8/17.
  */
 public class AddTestForMergeTwo {
+    public void method(){
+        System.out.println(111);
+        System.out.println(222);
+    }
 }
